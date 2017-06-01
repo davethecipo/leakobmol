ea57de7135f4db237c02de86c625ca565e538201
Davide Olianas <ubuntupk@gmail.com>
2017-06-01 22:07:41 +0200

    run ldconfig to enable finding of openbabel installed locally
    also, remove valgrind because I have difficulties running it inside VM

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,16 +5,17 @@ sudo apt-get update
 sudo apt-get -y upgrade
 
 
-sudo apt-get install -y git cmake build-essential pkg-config valgrind libopenbabel-dev
-#git clone git://github.com/openbabel/openbabel.git
-#cd openbabel
-#git checkout openbabel-2-4-1
-#mkdir build
-#cd build
-#cmake  -DCMAKE_BUILD_TYPE=Debug ..
-#make -j4
-#sudo make install
-#cd ../..
+sudo apt-get install -y git cmake build-essential pkg-config
+git clone git://github.com/openbabel/openbabel.git
+cd openbabel
+git checkout openbabel-2-4-1
+mkdir build
+cd build
+cmake  -DCMAKE_BUILD_TYPE=Debug ..
+make -j4
+sudo make install
+sudo ldconfig
+cd ../..
 git clone https://github.com/davethecipo/leakobmol
 cd leakobmol
 mkdir build
